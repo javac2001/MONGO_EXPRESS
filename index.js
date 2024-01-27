36966b9681f17304c2598d9f4d3e1d14d632d96b
Kaushik Roy <kaushikroykaushikroyezio9163@gmail.com>
2024-01-27 16:15:13 +0530

    STAGE=>3 Establish Express Connection

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+let express = require('express');
+let app = express();
+let port = 8080;
+let path = require("path");
+const methodOverride = require('method-override');
+
 let mongoose = require('mongoose');
 const { faker } = require('@faker-js/faker');
 let chat = require('./models/chat.js');
+
+
+// EXPRESS Setup
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
+// MongoDB Connection
 main()
 .then(()=>{
     console.log("CONNECTION ESTABLISHED");
@@ -12,3 +28,13 @@ main()
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp')
 }
+
+
+// Lising Port
+app.listen(port, ()=>{
+    console.log(`Lisining port ${port}`)
+})
+// Root
+app.get("/",(req, res)=>{
+    res.send("This is ROOT");
+})
